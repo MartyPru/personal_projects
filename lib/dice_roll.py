bf554d7baffeaa1f2126bbb72eb4d50685d9b965
Martynas Prusevicius <mprusevicius@gmail.com>
2023-11-15 08:48:30 +0000

    Add dice_roll.py with description of project

---
+++ lib/dice_roll.py
@@ -0,0 +1,19 @@
+# A project that allows the use to roll various combinations of dice
+# Using a pseudo-random number generator
+# Returning the result graphically and by text
+
+    # As a user
+    # So that I can decide outcomes
+    # I want to be able to roll dice
+
+    # As a user
+    # So that I can use different probabilities for my rolls
+    # I want to choose from a selection of different sided dice
+
+    # As a user
+    # So that I can follow the rules of my favourite tabletop game
+    # I want to be able to roll a combination of dice
+
+    # As a user
+    # So that I can see roll outcomes clearly
+    # I want a graphical representation of the outcomes of my rolls
